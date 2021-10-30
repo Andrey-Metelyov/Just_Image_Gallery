325b00fc0b679aedce0d54b9cca2c5e3443dabc7
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-10-30 14:16:31 +0900

    Problems with Glide on physical device

--- app/src/main/java/com/example/justimagegallery/BindingAdapters.kt
+++ app/src/main/java/com/example/justimagegallery/BindingAdapters.kt
@@ -1,24 +1,44 @@
 package com.example.justimagegallery
 
+import android.content.Context
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 import com.example.justimagegallery.network.PicsumPhoto
 import com.example.justimagegallery.overview.PhotoListAdapter
+
+@GlideModule
+class GlideApplication : AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.apply {
+            RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL).signature(
+                ObjectKey(System.currentTimeMillis().toShort())
+            )
+        }
+    }
+}
 
 @BindingAdapter("imageUrl")
 fun bindPhoto(imgView: ImageView, downloadUrl: String?) {
     downloadUrl?.let {
         val downloadUri = downloadUrl.toUri().buildUpon().scheme("https").build()
         System.err.println(downloadUri)
-        Glide.with(imgView.context)
+        GlideApp.with(imgView.context)
             .load(downloadUri)
-            .apply(RequestOptions()
-                .placeholder(R.drawable.ic_loading_image)
-                .error(R.drawable.ic_broken_image))
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.ic_loading_image)
+                    .error(R.drawable.ic_broken_image)
+            )
             .into(imgView)
     }
 }
