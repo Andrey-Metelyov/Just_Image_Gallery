6f8bbf2561c08b313e621989ed3d301c6a31cbc7
Andrey.Metelyov <andrey.metelyov@gmail.com>
2021-10-31 20:00:20 +0900

    Fixed physical device problem (limiting image size to 3040 pixels on largest side)

--- app/src/main/java/com/example/justimagegallery/network/PicsumPhoto.kt
+++ app/src/main/java/com/example/justimagegallery/network/PicsumPhoto.kt
@@ -3,6 +3,9 @@ package com.example.justimagegallery.network
 import android.os.Parcelable
 import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
+import kotlin.math.max
+
+const val MAX_IMAGE_SIZE = 3040
 
 @Parcelize
 data class PicsumPhoto(
@@ -12,4 +15,28 @@ data class PicsumPhoto(
     val height: Int,
     val url: String,
     @Json(name = "download_url") val downloadUrl: String,
-) : Parcelable
+) : Parcelable {
+    fun getAdaptedImageUrl(): String {
+        val maxDimension = max(width, height)
+        if (maxDimension > MAX_IMAGE_SIZE) {
+            val rate = MAX_IMAGE_SIZE.toDouble() / maxDimension
+            val h: Int
+            val w: Int
+            if (width == maxDimension) {
+                h = (height * rate).toInt()
+                w = MAX_IMAGE_SIZE
+            } else {
+                h = MAX_IMAGE_SIZE
+                w = (width * rate).toInt()
+            }
+            val newUrl = downloadUrl.substring(
+                0, downloadUrl.lastIndexOf('/', downloadUrl.lastIndexOf('/') - 1)
+            ) + '/' + w + '/' + h
+            System.err.println("newUrl = $newUrl")
+            return newUrl
+        } else {
+            System.err.println("downloadUrl = $downloadUrl")
+            return downloadUrl
+        }
+    }
+}
